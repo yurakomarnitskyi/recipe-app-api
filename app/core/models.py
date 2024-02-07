7426556f4f8030d621d7f7a5677b4bfc7584b616
Yura <komarnickijura@gmail.com>
2024-02-07 13:38:18 +0200

    User model update

--- app/core/models.py
+++ app/core/models.py
@@ -1,19 +1,32 @@
-"""Database models."""
+"""
+Database models.
+"""
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
-    PermissionsMixin
+    PermissionsMixin,
 )
 
 
 class UserManager(BaseUserManager):
-    """Manager for user."""
+    """Manager for users."""
 
     def create_user(self, email, password=None, **extra_fields):
-        """Create, sav and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
@@ -25,5 +38,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'

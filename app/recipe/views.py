759e1e3518ba6d756d46a8b5fca7b500cf839a4d
Yura <komarnickijura@gmail.com>
2024-02-21 16:00:42 +0200

    Update recipe api

---
+++ app/recipe/views.py
@@ -0,0 +1,35 @@
+"""
+Views for the reciper APIs
+"""
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APis."""
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrive recipes for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+    
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == 'list':
+            return serializers.RecipeSerializers
+        
+        return self.serializer_class
+    
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
+
+
+
+
